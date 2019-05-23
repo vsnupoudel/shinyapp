7982b4483c53dc5b54b67ce33b97e88691ad9e1c
unknown <replytobishnu@gmail.com>
2019-05-23 14:33:12 +0545

    Added documentation inside the shiny web app and added features

--- ui.R
+++ ui.R
@@ -9,17 +9,23 @@ shinyUI(fluidPage(
   # qsec (time for a quarter of a mile) and am (0=automatic, 1=manual)
   sidebarLayout(
     sidebarPanel(
-       numericInput("wt","Weight in 1000 lbs", value=2.5),
-       numericInput("qsec","Time for quarter of a mile", value=15),
+       numericInput("wt","Weight in 1000 lbs; range is normally 1-6", value=2.5),
+       numericInput("qsec","Time for quarter of a mile, range is 15-25 (seconds)", value=15),
        numericInput("am","Enter 0 for automatic and 1 for Manual", value=0),
        submitButton("Submit")
     ),
 
     mainPanel(
-      h3("The resulting mpg from the stats model is "),
-      textOutput("mpglm"),
-      h3("The resulting mpg from the caret model is "),
-      textOutput("mpgcaret"),
+      h3("Documentation for directions! The inputs are: "),
+      h6(" 1. Weight of the car in 1000 pounds"),
+      h6(" 2. Time for quarter of a mile in seconds"),
+      h6(" 3. 0 for automatic transmission and 1 for manual transmission"),
+      h6("==================================================================================="),
+      h3("The resulting mpg from the stats model is: "),
+      h2( textOutput("mpglm")),
+      h6("==================================================================================="),
+      h3("The resulting mpg from the caret model is: "),
+      h2( textOutput("mpgcaret")),
       h6("==================================================================================="),
       em("Plot with all the 3 variables against mpg "),
       h6("==================================================================================="),
